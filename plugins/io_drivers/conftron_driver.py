657d77b63cc3cd4e3d07bf2ba86c0398fa55339c
Matthew Peddie <mpeddie@gmail.com>
2011-03-11 01:30:14 -0800

    Gigantic revamp of conftron i/o system.  Note that this was synchronous with a lot of changes to the conftron project itself, including the introduction of the configuration api.

--- plugins/io_drivers/conftron_driver.py
+++ plugins/io_drivers/conftron_driver.py
@@ -1,9 +1,7 @@
 from enthought.traits.api import Str, Float, Range, Int, Bool, on_trait_change
 from enthought.traits.ui.api import View, Item
 import sys
-import os
-from xml.etree import ElementTree as ET
-import xml.parsers.expat as expat
+from os import environ
 from collections import deque
 
 from io_driver import IODriver
@@ -22,75 +20,43 @@ class ConftronDriver(IODriver):
   
   show_debug_msgs = Bool(True)
 
-  def lcm_handler(self, channel, data):
-
-    to_decoder = {}
-    to_decoder['class'] = self.messages[channel]['class']
-    to_decoder['name'] = self.messages[channel]['name']
-    to_decoder['type'] = self.messages[channel]['type']
-    to_decoder['channel'] = self.messages[channel]['channel']
-    to_decoder['message'] = self.messages[channel]['decoder'].decode(data)
-
-    self.queued_messages.append(to_decoder)
-
+  def subscribe_to_msg(self, msg, tcmod):
+    # This relies on the configuration library to make a handler that
+    # turns lcm structs into plot-o-matic dicts.  
+    hdl = self.conf.make_lcm_handler_from_telem(msg)
+    msgtype = getattr(tcmod, msg.type)
+    def msghdl(channel, data):
+      self.queued_messages.append(hdl(msgtype.decode(data)))
+    self.subs.append(self.lc.subscribe(msg.channel % msg, msghdl))
+    
   def open(self):
     self.queued_messages = deque([]) #fifo buffer
 
     # set up paths
-    self.ap_project_root = os.environ.get('AP_PROJECT_ROOT')
+    self.ap_project_root = environ.get('AP_PROJECT_ROOT')
     if self.ap_project_root == None:
       raise NameError("please set the AP_PROJECT_ROOT environment variable to use Conftron driver")
-    sys.path.append( self.ap_project_root+"/conftron/python" )
+    sys.path.append( self.ap_project_root+"/conftron/python/" )
+    sys.path.append( self.ap_project_root+"/conftron/" )
+    import configuration
 
     # create lcm instance
     import lcm
     self.lc = lcm.LCM()
 
-    # parse telemetry.xml
-    self.messages = {}
-    for cl in ET.ElementTree().parse( self.ap_project_root+"/conf/telemetry.xml").getchildren():
-      # import the class modules, like ap/sim/vis/etc
-      exec("import "+cl.attrib['name'])
-      class_module = eval(cl.attrib['name'])
-
-      if (not cl.attrib.has_key("plotomatic")) or (cl.attrib['plotomatic'] == 'ignore'):
-        for msg in cl.getchildren():
-          m = {}
-          # get the attributes right out of telemetry.xml
-
-          # first check to see if there is a "plotomatic" attribute
-          try:
-            pom_attribs = msg.attrib['plotomatic']
-          except KeyError:
-            pom_attribs = None
-
-          # ignore if plotomatic attribute is "ignore"
-          if pom_attribs == 'ignore':
-            continue
-
-          # otherwise proceed as normal
-          m['class'] = cl.attrib['name']
-          m['type'] = msg.attrib['type']
-          m['name'] = msg.attrib['name']
-
-          # create the channel name if one isn't explicitely specified
-          try:
-            m['channel'] = msg.attrib['channel']
-          except KeyError:
-            m['channel'] = m['class']+"_"+m['type']+"_"+m['name']
-
-          # get the decoder
-          m['decoder'] = eval("class_module."+m['type'])
-
-          # subscribe to the channel
-          m['subscription'] = self.lc.subscribe(m['channel'], self.lcm_handler )
-
-          # add this entry to the messages dictionary
-          self.messages[m['channel']] = m
+    self.conf = configuration.Configuration()
+    self.subs = []
+    for tc in self.conf.telemetry:
+      tcmod = __import__(tc.classname)
+      for msg in tc.messages:
+        if not msg.has_key('plotomatic'):
+          self.subscribe_to_msg(msg, tcmod)
+        if msg.has_key('plotomatic') and msg.plotomatic != 'ignore':
+          self.subscribe_to_msg(msg, tcmod)
 
   def close(self):
-    for msg in self.messages.values():
-      self.lc.unsubscribe(msg['subscription'])
+    for s in self.subs:
+      self.lc.unsubscribe(s)
 
   def pop_queue(self):
     # empty self.queued_messages
